4cb2164396e51b212d8002ebc0d1e33c80185789
minhtuzz1 <tuyetphamthanhanh@gmail.com>
2024-07-20 19:49:39 +0700

    Create page splash

--- food_delivery/lib/main.dart
+++ food_delivery/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/controllers/recommended_product_controller.dart';
-import 'package:food_delivery/pages/splash/splash_page.dart';
 import 'package:food_delivery/routes/routes_helper.dart';
 import 'package:get/get.dart';
 
@@ -24,8 +23,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
 
-      home: SplashScreen(),
-      //initialRoute: RouteHelper.getInitial(),
+      //home: SplashScreen(),
+      initialRoute: RouteHelper.getSplashPage(),
       getPages: RouteHelper.routes,
     );
   }
--- food_delivery/lib/pages/splash/splash_page.dart
+++ food_delivery/lib/pages/splash/splash_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:food_delivery/routes/routes_helper.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) :super(key: key);
@@ -17,10 +21,16 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
     super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2));
+      duration: const Duration(seconds: 2))..forward();
+      
     animation = CurvedAnimation(
       parent: controller,
       curve: Curves.linear);
+
+    Timer(
+      Duration(seconds: 3),
+      ()=> Get.offNamed(RouteHelper.getInitial())
+    );
   }
 
   @override
@@ -29,7 +39,10 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(child: Image.asset("assets/image/logo1.png", width: 250,))
+          ScaleTransition(
+            scale: animation,
+            child: Center(child: Image.asset("assets/image/logo1.png", width: 250,))),
+          Center(child: Image.asset("assets/image/logo2.png", width: 250,)),
         ],
       ),
     );
